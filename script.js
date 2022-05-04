08aefd9ad13171e61f2d48e49939a36a6d084dbc
Polina Boretc <101650145+Polinaboretc@users.noreply.github.com>
2022-05-04 09:47:59 +0200

    done

---
+++ script.js
@@ -0,0 +1,5 @@
+const tank = new Tank('panzer IV', 't', 'Krupp', 1935, 't', 50000, [43, 37], true, 'Acciao')
+console.log(tank.toString());
+
+const aircraft = new Aircraft('B52', 'a', 'Boeing', 1950, 9250000, [850, 700], false, 11000)
+console.log(aircraft.toString());
